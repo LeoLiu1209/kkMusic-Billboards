0e12619003f9f097f9dd009a68a4d26eee0a7ec4
leoliu <cubemail88@gmail.com>
2018-03-22 22:10:13 +0800

    setup for the express.js server

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(bodyParse.json())
+app.use(cors())
+
+app.get('/status', (req, res)=>{
+    res.send({
+        message: 'hello world'
+    })
+})
+
+app.listen(process.env.PORT||8081)
